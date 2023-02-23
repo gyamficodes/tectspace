122a3935cd2eb8407045ddb114ec3734a0f3942c
gyamficodes <gyamfijohn08@gmail.com>
2023-02-22 23:41:18 -0800

    call section start

---
+++ src/components/CallSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="call-background">
+    <div class="call-content-menu">
+      <div class="call-content">
+        <h1>Hey! Do You Need any Kinds <br> of Business Projects?</h1>
+        <div class="btn">
+            <span>get started now</span>
+            <font-awesome-icon class="myicon" icon="fa-solid fa-search" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "CallSection",
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  mounted() {},
+  watch: {},
+};
+</script>
+
+<style lang="scss">
+@import "../assets/callSection";
+</style>
