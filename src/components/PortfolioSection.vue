c27adf49ef6904a1f1a50f614d3fbb08db24d457
gyamficodes <gyamfijohn08@gmail.com>
2023-02-24 22:36:37 -0800

    card section update

--- src/components/PortfolioSection.vue
+++ src/components/PortfolioSection.vue
@@ -14,31 +14,53 @@
               </h1>
             </div>
             <div class="port-top-flex-b">
-                <div class="port-card" v-for="item in MyCards" :key="item.id">
-                    <span>{{ item.text }}</span>
-                </div>
+              <div class="port-card" v-for="item in MyCards" :key="item.id">
+                <span>{{ item.text }}</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="portfolio-down"></div>
+      <div class="portfolio-down">
+        <div class="portfolio-down-cards">
+          <div class="card">
+            <div :style="`background:url(${portA})`" class="img">
+              <div class="around">
+                <font-awesome-icon class="myicon" icon="fa-solid fa-search" />
+              </div>
+            </div>
+            <div class="card-content"></div>
+          </div>
+        </div>
+        <!-- card-down -->
+        <!-- <div class="portfolio-down-cards">
+          <div class="card">
+            <img src="" alt="">
+            <div class="card-content"></div>
+          </div>
+        </div> -->
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import port1 from "../assets/image/port1.png";
+
 export default {
   name: "PortfolioSection",
   props: {},
   data() {
     return {
-        MyCards: [
-        {id: 1, text: 'all'},
-        {id: 2, text: 'Branding'},
-        {id: 3, text: 'Design'},
-        {id: 4, text: 'Dashboard'},
-       {id: 5, text: 'Logo'},
-        ]
+      portA: port1,
+      show: false,
+      MyCards: [
+        { id: 1, text: "all" },
+        { id: 2, text: "Branding" },
+        { id: 3, text: "Design" },
+        { id: 4, text: "Dashboard" },
+        { id: 5, text: "Logo" },
+      ],
     };
   },
   methods: {},
