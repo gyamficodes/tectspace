97e092db7c350ac280374d2cbdeaa4a94075da12
gyamficodes <gyamfijohn08@gmail.com>
2023-03-06 22:45:50 -0800

    test-sec updated

--- src/components/TestimonialPage.vue
+++ src/components/TestimonialPage.vue
@@ -4,7 +4,31 @@
       <div class="test-menu">
         <div class="testi-menu-image">
          <img class="image" :src="menu" alt="">
-         <div class="round">
+
+        </div>
+        <div class="testi-menu-content">
+          <div class="box-tex">
+            Client Testimonial
+          </div>
+          <h1>Techno all Over World 23M+ <br> Satisfied Customers</h1>
+          <p>“I am Very Happy for the Awesome Service!”</p>
+          <h6>Completely strategize client-centric niche markets for forward platform <br> Phosfluorescently iterate efficient internal or "organic" sources wherea <br> revolutionary service. Objectively syndicate crosplatform  data without <br> scalable deliverables. Holisticly enhance.</h6>
+          <div class="flex-content">
+            <div class="flex-a">
+              <img class="pic" :src="pic1" alt="">
+              <div class="text">
+                <h4>Martin Collings</h4>
+                <p>CEO, Founder</p>
+              </div>
+            </div>
+            <div class="flex-b">
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="round">
             <div class="round-box">
                 <span>4.7</span>
                 <div class="stars">
@@ -16,16 +40,13 @@
                 <p>ratings</p>
             </div>
          </div>
-        </div>
-        <div class="testi-menu-content">qdwdqw</div>
-      </div>
-    </div>
   </section>
 </template>
 
 <script>
 import backg from "../assets/image/mybackground.png";
 import test from "../assets/image/test1.png";
+import pic from "../assets/image/people.png"
 
 export default {
   name: "TestimonialPage",
@@ -33,6 +54,7 @@ export default {
     return {
       testi: backg,
       menu: test,
+      pic1: pic,
     };
   },
   methods: {},
