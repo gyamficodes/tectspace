dd253467cd9017bcec0a7a183c4c88738a562960
gyamficodes <gyamfijohn08@gmail.com>
2023-03-08 20:19:20 -0800

    blog-section update finished

--- src/components/BlogSection.vue
+++ src/components/BlogSection.vue
@@ -23,10 +23,35 @@
       </div>
       <div class="blog-down">
         <div class="card-flex">
-            <div class="card">
-               <div class="back-image" style="background: url(../assets/image/blog1.png);"></div>
-                <div class="card-body"></div>
+          <div class="card" v-for="item in cards" :key="item.id">
+            <div
+              class="back-image"
+              :style="`background-image: url(${item.pic1})`"
+            ></div>
+            <div class="card-body">
+              <div class="card-flex">
+                <div class="im"><img :src="item.pic2" alt=""></div>
+                <span> {{ item.date }} </span>
+              </div>
+              <a href="#">
+                {{ item.a }}
+              </a>
+              <p>
+               {{ item.title }}
+              </p>
+              <div class="line"></div>
+              <div class="card-main-box">
+                <div class="main-box-a">
+                  <img :src="item.pic3" alt="" />
+                  <span>{{ item.name }} </span>
+                </div>
+                <div class="main-box-b">
+                  <span>{{ item.learn }}</span>
+                  <font-awesome-icon class="myicon" icon="fa-solid fa-search" />
+                </div>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </div>
@@ -34,10 +59,56 @@
 </template>
 
 <script>
+import image1 from "../assets/image/blog1.png";
+import image2 from "../assets/image/date.png";
+import image3 from "../assets/image/target.png";
+import image4 from "../assets/image/blog2.png";
+import image5 from "../assets/image/blog3.png";
+
 export default {
   name: "BlogSection",
   data() {
-    return {};
+    return {
+      pic3: image3,
+      cards: [
+        {
+          id:1,
+          pic1: image1,
+          pic2: image2,
+          date: " August 21, 2023 ",
+          a: "Top 10 Most Populars Google Chrome Extensions",
+          title:
+            "Monotonectally actualize <br> customer  methodologie rather than",
+            pic3:image3,
+            name:'Michel Das',
+            learn: 'learn more'
+        },
+        {
+          id:2,
+          pic1: image4,
+          pic2: image2,
+          date: " August 21, 2023 ",
+          a: " Most Important Figma  Tools Discussion",
+          title:
+            "Monotonectally actualize <br> customer  methodologie rather than",
+            pic3:image3,
+            name:'Michel Das',
+            learn: 'learn more'
+        },
+        {
+          id:3,
+          pic1: image5,
+          pic2: image2,
+          date: " August 21, 2023 ",
+          a: " How to Plugin Install  in Wordpress Server? ",
+          title:
+            "Monotonectally actualize <br> customer  methodologie rather than",
+            pic3:image3,
+            name:'Michel Das',
+            learn: 'learn more'
+        },
+      ],
+    };
   },
   methods: {},
   computed: {},
