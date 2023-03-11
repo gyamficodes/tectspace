848b732810944c78c7412b3ce8ac200e5d6c0024
gyamficodes <gyamfijohn08@gmail.com>
2023-03-10 19:53:05 -0800

    email-section section updated

--- src/components/SubscribePage.vue
+++ src/components/SubscribePage.vue
@@ -2,22 +2,22 @@
   <section class="Subscribe-page">
     <div class="container">
       <div class="sub-memu">
-        <div
-          class="main-back"
-          :style="` background-image: url(${imageA});`">
-       <div class="main-menu">
-        <div class="main-a">
-            <h2>Subscribe Our Newsletter</h2>
-            <p>Subscribe to Keep Up to Date with Everything Techno</p>
-            <div class="myBox">
+        <div class="main-back" :style="` background-image: url(${imageA});`">
+          <div class="main-menu">
+            <div class="main-a">
+              <h2>Subscribe Our Newsletter</h2>
+              <p>Subscribe to Keep Up to Date with Everything Techno</p>
+              <div class="myBox">
                 <div class="myBox-menu">
-                    <input type="text" placeholder="enter your email">
-                    <div class="btn"></div>
+                  <input type="text" placeholder="enter your email" />
+                  <div class="btn">Subscribe</div>
                 </div>
+              </div>
             </div>
-        </div>
-        <div class="main-b"><h1>dasvsgvdfgvfd</h1></div>
-       </div>
+            <div class="main-b">
+                <img :src="messages" alt="">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -26,12 +26,14 @@
 
 <script>
 import back from "../assets/image/subcrib-bg.png";
+import sms from "../assets/image/messege.png";
 
 export default {
   name: "SubscribePage",
   data() {
     return {
       imageA: back,
+      messages: sms,
     };
   },
   methods: {
