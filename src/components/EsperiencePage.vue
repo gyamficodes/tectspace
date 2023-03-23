140b052ba2a4312810dc6ba86acee534cd1caba4
gyamficodes <gyamfijohn08@gmail.com>
2023-03-23 12:14:38 -0700

    esperience page finihed

--- src/components/EsperiencePage.vue
+++ src/components/EsperiencePage.vue
@@ -8,16 +8,28 @@
         <div class="esperince-right">
           <div class="esperience-content">
             <h6>30 YEARS OF EXPERIENCE</h6>
-            <h1>Preparing For Your Success <br> Provide Best <span>IT Solutions.</span></h1>
+            <h1>Preparing For Your Success Provide Best <span>IT Solutions.</span></h1>
              <div class="h-line"></div>
              <p>We are privileged to work with hundred future thinking awesom <br> business including many of the world’s top hardware</p>
             <div class="flex-espe">
                 <div class="icon">
+                    <span class="material-icons">lock</span>
+                </div>
+                <div class="image-test">
+                    <h5>Warranty Management</h5>
+                    <p>Morem Ipsum is simply dummy text of the printing and <br/> pepesetting found.</p>
+                </div>
+            </div>
+            <div class="flex-espe">
+                <div class="icon">
                     <span class="material-icons">integration_instructions</span>
                 </div>
-                <div class="image-test"></div>
+                <div class="image-test">
+                    <h5>Quality Control System</h5>
+                    <p>Morem Ipsum is simply dummy text of the printing and <br/> pepesetting found.</p>
+                </div>
             </div>
-            
+
           </div>
         </div>
       </div>
