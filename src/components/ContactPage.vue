b25af0cc49c3c21f8d70a70991aa3b31ae9f0c7a
gyamficodes <gyamfijohn08@gmail.com>
2023-03-31 00:16:03 -0700

    Contact Page Updated

--- src/components/ContactPage.vue
+++ src/components/ContactPage.vue
@@ -5,12 +5,27 @@
         <div class="contact-main-box">
           <h1>contact us</h1>
           <div class="input-box">
-           <div class="ipA">
-            <input type="text" placeholder="Name">
-           </div>
-           <div class="ipB"></div>
+            <div class="ipA">
+              <input type="text" placeholder="Name">
+            </div>
+            <div class="ipB">
+              <input type="text" placeholder="Email">
+            </div>
           </div>
-
+          <div class="input-box">
+            <div class="ipA">
+              <input type="text" placeholder="Phone Number">
+            </div>
+            <div class="ipB">
+              <input type="text" placeholder="Website">
+            </div>
+          </div>
+              <div class="text-area">
+                <textarea name="text" id="" cols="30" rows="10" placeholder="Write Message"></textarea>
+              </div>
+              <div class="btn">
+                <span>send Message</span>
+              </div>
         </div>
       </div>
     </div>
